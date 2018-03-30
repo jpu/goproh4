532263ebaf057368446631488dbed93f7c2da413
jsk909 <jsk909@gmail.com>
2018-03-31 00:37:08 +0300

    status example: migrate to async-await

--- examples/status.js
+++ examples/status.js
@@ -1,18 +1,38 @@
 var GoPro = require('../lib/index.js');
-
-var cam = new GoPro.Camera();
-
+var cam = new GoPro.Camera({maxRetries: 1, timeout: 2000});
 
 /*
 **  Read one state
 */
-cam.status(GoPro.Status.InternalBatteryLevel).then(function (battery_level) {
-    console.log('[battery level] = ', battery_level);
-});
+getStatusBatteryAsync = async function (){
+    try {
+        let battery_level = await cam.status(GoPro.Status.InternalBatteryLevel);
+        console.log('[battery level] = ', battery_level);
+    } catch(err){
+        console.log("error: " +err);
+    }
+}
 
 /*
 **  Read all the status
 */
-cam.status().then(function (status) {
-    console.log('[status] = ', status);
-});
+getStatusAllAsync = async function (){
+    try {
+        let status = await cam.status();
+        console.log('[status] = ', status);
+    } catch(err){
+        console.log("error: " +err);
+    }
+}
+
+/*
+**  Main
+*/
+mainAsync = async function() {
+    console.log("getting battery level ...");
+    await getStatusBatteryAsync();
+    console.log("getting all status info ...");
+    await getStatusAllAsync();
+    console.log("end");
+}();
+
